e151e9948c5dbca40bd67aa95ac9df30e8f7d0bb
Andrew T. Lim <lim243@purdue.edu>
2020-09-21 23:33:09 -0400

    Add morgan and move db to separate file

--- server/index.js
+++ server/index.js
@@ -1,30 +1,23 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { Client } = require("pg");
+const morgan = require("morgan");
+const DB = require("./db.js");
 require("dotenv").config();
 
-//middleware
+// middleware
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev")); // For logging
 
-app.listen(5000, () => {
-  console.log("Server has already started at port 5000");
+// Routes
+app.get("/api/v1/", (req, res) => {
+  // Make whatever calls to DB
+  console.log("req", req.body);
 });
 
-// Database Client
-const db = new Client({
-  host: process.env.RDS_HOSTNAME,
-  port: process.env.RDS_PORTNUMBER,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-});
-
-// Connect to DB
-db.connect((err) => {
-  if (err) {
-    console.error("connection error", err.stack);
-  } else {
-    console.log("Connected to DB");
-  }
+// Start Server
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server has already started at port ${port}`);
 });
