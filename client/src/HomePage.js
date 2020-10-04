a3ec260dd76f05f332e5cc7826e95fd642986df6
Andrew T. Lim <lim243@purdue.edu>
2020-10-03 22:40:45 -0400

    Change to feed

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 //import './HomePage.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
@@ -11,33 +11,35 @@ import Forgot from "./components/ForgotPassword";
 import Restore from "./components/RestorePassword";
 import Reset from "./components/ResetPassword";
 import { LoggedIn } from "./LoggedIn";
+import { Feed } from "./Feed";
 
 function HomePage() {
-    return (
+  return (
     <Router>
-        <NavigationBar />
-        <br></br>
-        <br></br>
-        <div className="HomePage">
-            <header className="HomePage-header">
-                <div className="auth-wrapper">
-                    <div className="auth-inner">
-                        <Switch>
-                            <Route exact path= '/' component={Login} />
-                            <Route path="/sign-in" component={Login} />
-                            <Route path="/sign-up" component={Signup} />
-                            <Route path="/help" component={Help} />
-                            <Route path="/forgot" component={Forgot} />
-                            <Route path="/restore" component={Restore} />
-                            <Route path="/account" component={LoggedIn} />
-                            <Route path="/reset" component={Reset} />
-                        </Switch>
-                    </div>
-                </div>
-            </header>
-        </div>
+      <NavigationBar />
+      <br></br>
+      <br></br>
+      <div className='HomePage'>
+        <header className='HomePage-header'>
+          <div className='auth-wrapper'>
+            <div className='auth-inner'>
+              <Switch>
+                <Route exact path='/' component={Login} />
+                <Route path='/sign-in' component={Login} />
+                <Route path='/sign-up' component={Signup} />
+                <Route path='/help' component={Help} />
+                <Route path='/forgot' component={Forgot} />
+                <Route path='/restore' component={Restore} />
+                <Route path='/account' component={LoggedIn} />
+                <Route path='/reset' component={Reset} />
+                <Route path='/feed' component={Feed} />
+              </Switch>
+            </div>
+          </div>
+        </header>
+      </div>
     </Router>
-    );
+  );
 }
 
 export default HomePage;
