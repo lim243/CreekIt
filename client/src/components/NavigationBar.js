658482e7bb5325429604c696295c12d33f76431e
Andrew T. Lim <lim243@purdue.edu>
2020-10-07 21:12:44 -0400

    Done with search bar

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
-import styled from 'styled-components';
+import React from "react";
+import { Nav, Navbar, Form, FormControl, Button, Row, Col } from "react-bootstrap";
+import styled from "styled-components";
+import { Redirect } from "react-router-dom";
 
 const Styles = styled.div`
-  .navbar { background-color: #222; }
-  a, .navbar-nav, .navbar-light .nav-link {
-    color: #9FFFCB;
-    &:hover { color: white; }
+  .navbar {
+    background-color: #222;
+  }
+  a,
+  .navbar-nav,
+  .navbar-light .nav-link {
+    color: #9fffcb;
+    &:hover {
+      color: white;
+    }
   }
   .navbar-brand {
     font-size: 2.1em;
-    color: #9FFFCB;
-    &:hover { color: white; }
-    margin-left:15px !important;
+    color: #9fffcb;
+    &:hover {
+      color: white;
+    }
+    margin-left: 15px !important;
   }
   .form-center {
     position: absolute !important;
@@ -26,30 +35,91 @@ const Styles = styled.div`
     width: 100%;
     z-index: 8;
   }
-
 `;
 
+class NavigationBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "",
+      redirectUser: false,
+      redirectTopic: false,
+    };
+  }
 
-export const NavigationBar = () => (
-    <Styles>
-        <div className="sticky">
-      <Navbar expand="lg">
-        <Navbar.Brand href="/feed">CreekIt</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-        <Form className="form-center">
-          <FormControl type="text" placeholder="Search" className="" />
-        </Form>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Item><Nav.Link href="/feed/myprofile">My Profile</Nav.Link></Nav.Item> 
-            <Nav.Item><Nav.Link href="/help">FAQ</Nav.Link></Nav.Item> 
-            <Nav.Item><Nav.Link href="/logout">Logout</Nav.Link></Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      </div>
-    </Styles>
-  )
+  handleChange = (event) => {
+    console.log("event.target.value", event.target.value);
+    this.setState({ search: event.target.value });
+  };
 
-  export default NavigationBar;
+  handleSubmit = (event) => {
+    console.log("Hi");
 
+    const searchString = this.state.search;
+    if (searchString.startsWith("#")) {
+      // Search Topic
+      this.setState({ redirectTopic: true, redirectUser: false });
+    } else {
+      // Search User
+      this.setState({ redirectUser: true, redirectTopic: false });
+    }
+
+    event.preventDefault();
+  };
+
+  render() {
+    if (this.state.redirectTopic) {
+      console.log("this.state", this.state);
+      return (
+        <Redirect
+          to={{
+            pathname: "/feed/topic/" + this.state.search.substring(1),
+          }}
+        />
+      );
+    } else if (this.state.redirectUser) {
+      return (
+        <Redirect
+          to={{
+            pathname: "/feed/myprofile/" + this.state.search,
+          }}
+        />
+      );
+    }
+
+    return (
+      <Styles>
+        <div className='sticky'>
+          <Navbar expand='lg'>
+            <Navbar.Brand href='/feed'>CreekIt</Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Form inline className='form-center'>
+              <FormControl
+                type='text'
+                placeholder='Search'
+                className=''
+                onChange={this.handleChange}
+              />
+              <Button onClick={this.handleSubmit}>Search</Button>
+            </Form>
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='ml-auto'>
+                <Nav.Item>
+                  <Nav.Link href='/feed/myprofile'>My Profile</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href='/help'>FAQ</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href='/logout'>Logout</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+      </Styles>
+    );
+  }
+}
+
+export default NavigationBar;
