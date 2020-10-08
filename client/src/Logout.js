5168d65d0b1a37e33481ca7e285a4e546882e78b
Shirong Zheng <zheng460@purdue.edu>
2020-10-08 01:37:38 -0400

    logout

--- client/src/Logout.js
+++ client/src/Logout.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import styled from "styled-components";
+import { withRouter } from "react-router-dom";
+import axios from 'axios'
 const GridWrapper = styled.div`
   display: grid;
   grid-gap: 10px;
@@ -9,11 +13,8 @@ const GridWrapper = styled.div`
   grid-template-columns: repeat(12, 1fr);
   grid-auto-rows: minmax(25px, auto);
 `; 
-export const Logout = () => (
-  <GridWrapper>
-    <h2>About Page</h2>
-    <p>State at ceiling lay on arms while you're using the keyboard so this human feeds me.</p>
-    <p>I am a kitty cat, sup, feed me, no cares in the world</p>
-    <p>Meow meow, I tell my human purr for no reason but to chase after</p>
-  </GridWrapper>
-)
+const Logout = () => {
+  localStorage.clear();
+  document.location.href = "http://localhost:3000/";
+}
+export default withRouter(Logout);
