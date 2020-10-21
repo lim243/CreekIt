ae933e0e37e8c5721b8498c14837f817d1fe1b74
Jason Tong <jasonptong@gmail.com>
2020-10-20 23:51:01 -0400

    Fixing login and signup verifications

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -108,7 +108,7 @@ const Signup = () => (
         username: Yup.string()
           .required("Required")
           //   .matches(/(^[\w+]*$)/, "Username cannot contain spaces."),
-          .matches(/(^[\w+@.]*$)/, "Username cannot contain spaces."), // TODO: Change this since i just hacked it for email and username being the same
+          .matches(/(^[\w+-.]*$)/, "Username cannot contain spaces and the following characters: @!?()#$%^&*"), // TODO: Change this since i just hacked it for email and username being the same
         date: Yup.string().required("Required"),
         //.matches(/^(?:(?:(?:0?[13578]|1[02])(\/|-|\.)31)\1|(?:(?:0?[1,3-9]|1[0-2])(\/|-|\.)(?:29|30)\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:0?2(\/|-|\.)29\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:(?:0?[1-9])|(?:1[0-2]))(\/|-|\.)(?:0?[1-9]|1\d|2[0-8])\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/, "Enter a valid date with the given format: MM/DD/YYYY."),
       })}
