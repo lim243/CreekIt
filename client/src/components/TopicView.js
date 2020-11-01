bc2d9cffaa7fd11386e010bdfafd677736b97188
Andrew T. Lim <lim243@purdue.edu>
2020-11-01 16:58:29 -0500

    Not found topics

--- client/src/components/TopicView.js
+++ client/src/components/TopicView.js
@@ -19,6 +19,7 @@ class TopicView extends React.Component {
     super(props);
     this.state = {
       posts: [],
+      topic: "",
     };
   }
 
@@ -27,6 +28,7 @@ class TopicView extends React.Component {
     const topic = url.split("/").pop(-1);
     console.log("url", url);
     console.log("This is topic:" + topic);
+    this.setState({ topic });
     this.fetchTopics(topic);
   }
 
@@ -43,31 +45,35 @@ class TopicView extends React.Component {
   };
 
   render() {
-    console.log("this.state", this.state);
+    console.log("this.state", this.state.posts.length);
 
     return (
       <GridWrapper>
         <br></br>
         <br></br>
         <br></br>
-        <h1>Topic</h1>
+        <h1>{`Topic: #${this.state.topic}`}</h1>
 
-        {this.state.posts.map((item, index) => (
-          <Post
-            key={index}
-            index={index}
-            anonymous={item.anonymous}
-            postId={item.post_id}
-            name={item.name}
-            username={item.username}
-            post={item.body}
-            date={item.date}
-            time={item.time}
-            upvotes={item.upvotes}
-            downvotes={item.downvotes}
-            topic={item.topic}
-          />
-        ))}
+        {this.state.posts.length === 0 ? (
+          <div>Topic does not exist</div>
+        ) : (
+          this.state.posts.map((item, index) => (
+            <Post
+              key={index}
+              index={index}
+              anonymous={item.anonymous}
+              postId={item.post_id}
+              name={item.name}
+              username={item.username}
+              post={item.body}
+              date={item.date}
+              time={item.time}
+              upvotes={item.upvotes}
+              downvotes={item.downvotes}
+              topic={item.topic}
+            />
+          ))
+        )}
       </GridWrapper>
     );
   }
