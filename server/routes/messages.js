6f0fa60507800894625fc5c3500253b7d590cc54
Andrew T. Lim <lim243@purdue.edu>
2020-11-27 22:25:27 -0500

    Change send message

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -8,6 +8,7 @@ module.exports = router;
 router.get("/:convoId", getConversationById);
 
 router.post("/createConvo", createConversation);
+router.post("/sendMessage", sendMessage);
 
 router.get("/text", async (req, res) => {
   //console.log("req.body", req.body);
@@ -86,49 +87,97 @@ async function getConversationById(req, res) {
  * POST FUNCTIONS
  * A list of setter functions
  */
+
 async function createConversation(req, res) {
   const { username_A, username_B } = req.body;
 
-  // 1. update users table convo list
-  // 2. create a new convo by sending a default message from A -> B
-  // 3. when B receives the msg, add convo id to convo list
-  const date = Date.now();
-  const newMessage = `Hi, I'm ${username_A}`;
-
   const query = {
     name: "create-new-conversation",
-    text: `INSERT INTO messages(date, username, message_body)
-      VALUES (to_timestamp($2/1000.0), $1, $3)
-      RETURNING conversation_id`,
-    values: [username_A, date, newMessage],
+    text: `INSERT INTO direct_message(username1, username2)
+      VALUES ($1, $2)
+      RETURNING id`,
+    values: [username_A, username_B],
   };
 
   db.query(query)
     .then((res1) => {
-      const convoId = res1.rows[0].conversation_id;
+      const convoId = res1.rows[0].id;
       console.log("convoId", convoId);
-
-      const query2 = {
-        name: "append-convo-list",
-        text: `update users set
-                convo_id = array_append(convo_id, cast($3 AS bigint))
-                where username in ($1, $2)`,
-        values: [username_A, username_B, convoId],
-      };
-
-      console.log("query2", query2);
-
-      db.query(query2)
-        .then((res2) => {
-          console.log("res2", res2);
-          const data = {
-            "success": true,
-            "payload": { conversation_id: convoId },
-          };
-
-          res.status(200).send(data);
-        })
-        .catch((err2) => console.error(err2));
+      const data = { success: true, payload: convoId };
+      res.status(200).send(data);
     })
     .catch((error) => console.error(error));
 }
+
+async function sendMessage(req, res) {
+  const { username_A, username_B, body, sender } = req.body;
+
+  const time = Date.now();
+
+  const query = {
+    name: "send-message",
+    text: `UPDATE direct_message 
+      SET body = array_append(body, cast($3 AS character varying)),
+        sender = array_append(sender, cast($4 AS character varying)),
+        times = array_append(times, to_timestamp($5/1000.0)),
+        last_updated_time = to_timestamp($5/1000.0)
+      WHERE username1 = $1 AND username2 = $2 OR username2 = $1 AND username1 = $2
+      RETURNING *`,
+    values: [username_A, username_B, body, sender, time],
+  };
+
+  db.query(query)
+    .then((res1) => {
+      const convoId = res1.rows[0].id;
+      console.log("convoId", res1.rows);
+      const data = { success: true, payload: convoId };
+      res.status(200).send(data);
+    })
+    .catch((error) => console.error(error));
+}
+// async function createConversation(req, res) {
+//   const { username_A, username_B } = req.body;
+
+//   // 1. update users table convo list
+//   // 2. create a new convo by sending a default message from A -> B
+//   // 3. when B receives the msg, add convo id to convo list
+//   const date = Date.now();
+//   const newMessage = `Hi, I'm ${username_A}`;
+
+//   const query = {
+//     name: "create-new-conversation",
+//     text: `INSERT INTO messages(date, username, message_body)
+//       VALUES (to_timestamp($2/1000.0), $1, $3)
+//       RETURNING conversation_id`,
+//     values: [username_A, date, newMessage],
+//   };
+
+//   db.query(query)
+//     .then((res1) => {
+//       const convoId = res1.rows[0].conversation_id;
+//       console.log("convoId", convoId);
+
+//       const query2 = {
+//         name: "append-convo-list",
+//         text: `update users set
+//                 convo_id = array_append(convo_id, cast($3 AS bigint))
+//                 where username in ($1, $2)`,
+//         values: [username_A, username_B, convoId],
+//       };
+
+//       console.log("query2", query2);
+
+//       db.query(query2)
+//         .then((res2) => {
+//           console.log("res2", res2);
+//           const data = {
+//             "success": true,
+//             "payload": { conversation_id: convoId },
+//           };
+
+//           res.status(200).send(data);
+//         })
+//         .catch((err2) => console.error(err2));
+//     })
+//     .catch((error) => console.error(error));
+// }
