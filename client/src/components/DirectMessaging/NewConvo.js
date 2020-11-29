dbf9d1b2896278d4c6c7be50d8c7cf3e0db78262
Andrew T. Lim <lim243@purdue.edu>
2020-11-29 17:25:59 -0500

    fetch users if not private

--- client/src/components/DirectMessaging/NewConvo.js
+++ client/src/components/DirectMessaging/NewConvo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from 'react-modal';
+import axios from 'axios';
 
 import './NewConvo.css';
 
@@ -19,13 +20,51 @@ class NewConvo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            modal2: "",
-            listFollowing: [{username:'bob'}, {username:'apple'}]
+            modal2: false,
+            listFollowing: [{username:'bob'}, {username:'apple'}],
+            private: false
         }
         this.modalOpen2 = this.modalOpen2.bind(this);
         this.modalClose2 = this.modalClose2.bind(this);
         this.handleNewDM = this.handleNewDM.bind(this);
+        this.fetchNewConvoUsers = this.fetchNewConvoUsers.bind(this);
+        this.fetchPrivacy = this.fetchPrivacy.bind(this);
     };
+
+    fetchNewConvoUsers(isPrivate){
+        const username = localStorage.getItem('username')
+
+        // let isPrivate = this.state.private
+        console.log('this.state.private', isPrivate);
+        if (isPrivate){
+            axios.get(`http://localhost:5000/api/v1/users/${username}/following`)
+                .then(res => {
+                    console.log('res', res.data.following);
+                    const following = res.data.following.map(ele => ({username: ele}))
+                
+
+                    this.setState({listFollowing: following})
+                }).catch(err => console.error(err))
+        } else {
+            axios.get(`http://localhost:5000/api/v1/users/${username}/getPublicUsers`)
+                .then(res => {
+                    console.log('res', res);
+                    const following = res.data
+                    this.setState({listFollowing: following})
+                }).catch(err => console.error(err))
+        }
+    }
+
+    async fetchPrivacy() {
+        const username = localStorage.getItem('username')
+        const res = await axios.get(`http://localhost:5000/api/v1/users/${username}/getPrivacy`)
+        return res.data.private
+    }
+
+    async componentDidMount() {
+        const isPrivate = await this.fetchPrivacy()
+        this.fetchNewConvoUsers(isPrivate);
+    }
 
     handleNewDM(event) {
         // Handle adding to conversation
@@ -56,7 +95,7 @@ class NewConvo extends React.Component {
                     <br></br>
                     <br></br>
                     {this.state.listFollowing.map((item, index) => (
-                        <div>
+                        <div key={index}>
                             <button id="dm" onClick={this.handleNewDM} >{item.username}</button>
                             <br></br>
                         </div>
